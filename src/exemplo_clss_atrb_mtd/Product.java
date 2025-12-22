804c408ce3f88f913ecf8bd55d66a8ecb23571a8
Eduardo Moreira Viter <moreiraviter@outlook.com>
2025-12-22 16:05:10 -0300

    criado exemplo 2 classe, metodo e atributo: product

---
+++ src/exemplo_clss_atrb_mtd/Product.java
@@ -0,0 +1,20 @@
+package exemplo_clss_atrb_mtd;
+
+public class Product {
+
+    public String name;
+    public double price;
+    public int quantity;
+
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+
+    public void addProducts(int quantity){
+        this.quantity += quantity;
+    }
+
+    public void removeProducts(int quantity){
+        this.quantity -= quantity;
+    }
+}
