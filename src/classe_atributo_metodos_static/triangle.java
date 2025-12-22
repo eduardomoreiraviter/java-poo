03435178b1ad78a033e6a71e4fc03bbe7fbf640c
Eduardo Moreira Viter <moreiraviter@outlook.com>
2025-12-22 09:35:42 -0300

    add: metodo area

--- src/classe_atributo_metodos_static/triangle.java
+++ src/classe_atributo_metodos_static/triangle.java
@@ -5,4 +5,9 @@ public class triangle {
     public double a;
     public double b;
     public double c;
+
+    public double area(){
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p- c));
+    }
 }
