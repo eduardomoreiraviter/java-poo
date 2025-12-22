a92b0ea72f9a848385bf91f36f236eb6b5bcc2e1
Eduardo Moreira Viter <moreiraviter@outlook.com>
2025-12-22 09:13:44 -0300

    first commit

---
+++ src/classe_atributo_metodos_static/app_Triangle.java
@@ -0,0 +1,44 @@
+package classe_atributo_metodos_static;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class app_Triangle {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        triangle x, y;
+        x = new triangle();
+        y = new triangle();
+
+        System.out.println("enter the measure of triangle X:");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        System.out.println("enter the measure of triangle Y:");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+        p = (y.a + y.b + y.c) / 2;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY){
+            System.out.println("Larger area: X");
+        } else {
+            System.out.println("Larger area: Y");
+        }
+
+
+
+        sc.close();
+    }
+}
---
+++ src/classe_atributo_metodos_static/triangle.java
@@ -0,0 +1,8 @@
+package classe_atributo_metodos_static;
+
+public class triangle {
+
+    public double a;
+    public double b;
+    public double c;
+}
