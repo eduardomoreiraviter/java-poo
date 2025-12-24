cf458317a31ba35e458747cc14c782405bd425c3
Eduardo Moreira Viter <moreiraviter@outlook.com>
2025-12-24 05:18:39 -0300

    add: construtor classe product

--- src/exemplo_clss_atrb_mtd/Program.java
+++ src/exemplo_clss_atrb_mtd/Program.java
@@ -8,22 +8,22 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
-
         System.out.println("Enter product data:");
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
 
         System.out.println();
         System.out.print("Product data" + product);
 
         System.out.println();
         System.out.print("Enter the number of products to be added in stock:");
-        int quantity = sc.nextInt();
+        quantity = sc.nextInt();
         product.addProducts(quantity);
 
         System.out.println();
@@ -36,12 +36,6 @@ public class Program {
 
         System.out.println();
         System.out.print("Updated data" + product);
-
-
-
-
-
-
 
         sc.close();
     }
